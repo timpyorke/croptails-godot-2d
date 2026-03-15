8730aacebe1027f7f26ff118da494c3c9fb10b93
Thongphitak Sowanna <thongphitak.tim@gmail.com>
2026-03-15 11:41:19 +0700

    Add tilemap scene and implement game input events
    - Created a new tilemap scene (`test_scence_tilemap.tscn`) with layers for Water, Glass, TilledDirt, and Nature, utilizing a shared TileSet resource.
    - Introduced `game_input_events.gd` to handle player movement input, including methods for detecting movement direction and checking if any movement input is active.
    - Defined a new `DataTypes` class with an enum for various tools in `data_types.gd`.
    - Established a state machine framework with `NodeState` and `NodeStateMachine` classes to manage state transitions and processing.

---
+++ scenes/charecters/player/chopping.gd
@@ -0,0 +1,20 @@
+extends NodeState
+
+func _on_process(_delta : float) -> void:
+	pass
+
+
+func _on_physics_process(_delta : float) -> void:
+	pass
+
+
+func _on_next_transitions() -> void:
+	pass
+
+
+func _on_enter() -> void:
+	pass
+
+
+func _on_exit() -> void:
+	pass
---
+++ scenes/charecters/player/player.gd
@@ -0,0 +1,5 @@
+class_name Player extends CharacterBody2D
+
+@export var current_tools: DataTypes.Tools = DataTypes.Tools.None
+
+var player_direction: Vector2
